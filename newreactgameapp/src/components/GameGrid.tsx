377ad59a6c099c17c953de106e47ab72a86f93d7
Johnnypham2 <phammjohnny@gmail.com>
2024-06-17 19:16:32 -0700

    added hooks, gameCard etc

--- newreactgameapp/src/components/GameGrid.tsx
+++ newreactgameapp/src/components/GameGrid.tsx
@@ -1,48 +1,17 @@
-import { useEffect, useState } from "react"
-import apiClient from "./services/apiClient"
-import { Text } from "@chakra-ui/react"
+import { SimpleGrid, Text } from "@chakra-ui/react"
+import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 
-//help us shape our data in the form of our interfaces (type) props to pass data from parent component to child
-interface Game {
-    id: number
-    name: string
-}
-
-interface FetchGameResponse {
-    count: number
-    results: Game []
-}
 
 const GameGrid = () => {
-
-//useStates to help us render update our UI with our games and others
-const [games, setGames] = useState<Game[]>([]);
-const [error, setError] = useState();
-
-//create a helper function to help us fetch our code
-// '/games' is our endpoint
-const fetchGames = () => {
-    apiClient.get('/gamess')
-    .then(response => setGames(response.data.results))
-    .catch(error => {
-        setError(error.message)
-    })
-}
-
-//useEffect to fetch our data
-useEffect(() => {
-
-fetchGames();
-    
-}, [])
-
+const {games, error} = useGames();
 
   return (
     <>
-    <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>
+    <SimpleGrid>
+        {games.map(game => <GameCard game={game} key={game.id}></GameCard>)}
+    </SimpleGrid>
     {error && <Text color={'red'}>{error}</Text>}
     </>
   )
