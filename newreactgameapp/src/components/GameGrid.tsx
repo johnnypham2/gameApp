6dd22d59c4a1d633f0297d7731f2d2c85230c223
Johnnypham2 <phammjohnny@gmail.com>
2024-10-22 18:49:31 -0700

    scrolling

--- newreactgameapp/src/components/GameGrid.tsx
+++ newreactgameapp/src/components/GameGrid.tsx
@@ -4,18 +4,14 @@
 // import apiClient from "../services/apiClient"
 // import { Genre } from "../hooks/useGenres"
 // import { Platform } from "../hooks/usePlatforms"
-import { Button, SimpleGrid, Text } from "@chakra-ui/react"
+import { Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
 import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
 import { GameQuery } from "../App"
 import React from "react"
-
-// sent to useGames.ts
-// stuff here
-
-// props to pass data from parent component to child component 
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface Props {
 //refactored
@@ -26,17 +22,22 @@ interface Props {
 
 const GameGrid = ({gameQuery}:Props) => {
 
-   // we have cut the usestate , our fetch, and useEffect and place them into our custom hook
-   // and now we simply use our custom hook useGames() hook
+   
    const {data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage} = useGames(gameQuery);
 
     const skeleton = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-    // we may have other helper functions to add, delete or update data
+
+   const fetchedGameCount = data?.pages.reduce((total,page) => total + page.results.length,0) || 0
+
   return (
     <>
-        {/* display our data  ul  and li grid table usually a map with a unique key */}
-        {/*  remember to use {} to do any logic */}
-                                      {/* every {1} is equal to 4 pixels */}
+       <InfiniteScroll
+       dataLength={fetchedGameCount}
+       hasMore={!!hasNextPage}
+       next={() => fetchNextPage()}
+       loader={<Spinner/>}
+       >
+
         <SimpleGrid columns={{sm:1,md:2,lg:3,xl:4}} 
                     spacing={3} 
                     padding={5}
@@ -47,7 +48,7 @@ const GameGrid = ({gameQuery}:Props) => {
               <GameCardSkeleton/>
             </GameCardContainer>
             
-            )}
+          )}
 
             {data?.pages.map((page,index) => 
                 <React.Fragment key={index}>
@@ -60,6 +61,7 @@ const GameGrid = ({gameQuery}:Props) => {
             )}
 
         </SimpleGrid>
+            </InfiniteScroll>
         {hasNextPage && <Button onClick={() => fetchNextPage()}>{isFetchingNextPage ? 'Loading...' : 'Load More'}</Button>}
 
         {/* render errors */}
